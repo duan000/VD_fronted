6d1894e64e58392c17d1c59d6d7a527eef3ab848
linkstecVD <Linkage@12345>
2022-01-21 16:12:00 +0900

    顧客カードバター制御+package

--- src/app/pages/custom-card-input/custom-card-input.component.ts
+++ src/app/pages/custom-card-input/custom-card-input.component.ts
@@ -1,14 +1,58 @@
-import { ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BasicPageComponent } from 'src/app/common/basicPage';
 import { CustCardInService } from 'src/app/service/cust-card-in.service';
 
 const ELEMENT_DATA: any[] = [
-
-  { name: '年收入', list_td: ['なし', '240万円以下', '510万円以下', '720万円以下', '2400万円以下', '3400万円以下', '540万円以下', '1億以内'], value: '' },
-  { name: '金融資産', list_td: ['なし', '240万円以下', '510万円以下', '720万円以下', '2400万円以下', '3400万円以下', '640万円以下', '7400万円以下', '9500万円以下', '2億以内'], value: '' },
-  { name: '投資方針', list_td: ['なし', '240万円以下', '510万円以下', '720万円以下', '2400万円以下'], value: '' },
+  {
+    name: '年收入',
+    list_td: [
+      'なし',
+      '240万円以下',
+      '510万円以下',
+      '720万円以下',
+      '2400万円以下',
+      '3400万円以下',
+      '540万円以下',
+      '1億以内',
+    ],
+    value: '',
+  },
+  {
+    name: '金融資産',
+    list_td: [
+      'なし',
+      '240万円以下',
+      '510万円以下',
+      '720万円以下',
+      '2400万円以下',
+      '3400万円以下',
+      '640万円以下',
+      '7400万円以下',
+      '9500万円以下',
+      '2億以内',
+    ],
+    value: '',
+  },
+  {
+    name: '投資方針',
+    list_td: [
+      'なし',
+      '240万円以下',
+      '510万円以下',
+      '720万円以下',
+      '2400万円以下',
+    ],
+    value: '',
+  },
 ];
 
 const ELEMENT_DATA1: any[] = [
@@ -21,28 +65,27 @@ const ELEMENT_DATA1: any[] = [
   { list_name: '公社俸', value: '' },
   { list_name: '投資信託', value: '' },
   { list_name: 'その他', value: '' },
-]
+];
 
 @Component({
   selector: 'app-custom-card-input',
   templateUrl: './custom-card-input.component.html',
-  styleUrls: ['./custom-card-input.component.css']
+  styleUrls: ['./custom-card-input.component.css'],
 })
-export class CustomCardInputComponent extends BasicPageComponent implements OnInit {
-
-  constructor(
-    private router: Router,
-    private custService: CustCardInService,
-  ) {
+export class CustomCardInputComponent
+  extends BasicPageComponent
+  implements OnInit
+{
+  constructor(private router: Router, private custService: CustCardInService) {
     super();
   }
 
-  investmentExperience = ['経験なし', '１年未満', '５年未満', '５年以上']
+  investmentExperience = ['経験なし', '１年未満', '５年未満', '５年以上'];
   dataSource = ELEMENT_DATA;
   // input_1-7绑定的值
   dataSource1 = ELEMENT_DATA1;
 
-  // ボタン制御 
+  // ボタン制御
   isShow: boolean = false;
 
   public btnCheck = {
@@ -58,39 +101,50 @@ export class CustomCardInputComponent extends BasicPageComponent implements OnIn
     InvestmentExperience_7: '',
     InvestmentExperience_8: '',
     InvestmentExperience_9: '',
-  }
+  };
 
   watchValue() {
-
-    if (!(this.btnCheck.annualIncome === '' || this.btnCheck.financialAssets === '' ||
-      this.btnCheck.investmentPolicy === '' || this.btnCheck.InvestmentExperience_1 === '' ||
-      this.btnCheck.InvestmentExperience_2 === '' || this.btnCheck.InvestmentExperience_3 === '' ||
-      this.btnCheck.InvestmentExperience_4 === '' || this.btnCheck.InvestmentExperience_5 === '' ||
-      this.btnCheck.InvestmentExperience_6 === '' || this.btnCheck.InvestmentExperience_7 === '' ||
-      this.btnCheck.InvestmentExperience_8 === '' || this.btnCheck.InvestmentExperience_9 === '')
+    if (
+      !(
+        this.btnCheck.annualIncome === '' ||
+        this.btnCheck.financialAssets === '' ||
+        this.btnCheck.investmentPolicy === '' ||
+        this.btnCheck.InvestmentExperience_1 === '' ||
+        this.btnCheck.InvestmentExperience_2 === '' ||
+        this.btnCheck.InvestmentExperience_3 === '' ||
+        this.btnCheck.InvestmentExperience_4 === '' ||
+        this.btnCheck.InvestmentExperience_5 === '' ||
+        this.btnCheck.InvestmentExperience_6 === '' ||
+        this.btnCheck.InvestmentExperience_7 === '' ||
+        this.btnCheck.InvestmentExperience_8 === '' ||
+        this.btnCheck.InvestmentExperience_9 === ''
+      )
     ) {
-      this.isShow = true
+      this.isShow = true;
     } else {
-      this.isShow = false
+      this.isShow = false;
     }
   }
 
   ngOnInit(): void {
-    console.log(this.dataSource)
+    console.log(this.custService.radioValue);
+    this.btnCheck = this.custService.radioValue;
+    this.watchValue();
+    
   }
 
   nextPage() {
+    this.custService.radioValue = this.btnCheck;
 
     this.custService.dataSource1 = this.dataSource1;
     this.custService.radioValue = this.btnCheck;
-    console.log(this.custService.dataSource1)
-    console.log(this.custService.radioValue)
+    console.log(this.custService.dataSource1);
+    console.log(this.custService.radioValue);
 
-    this.router.navigate(['pages/fundList'])
+    this.router.navigate(['pages/fundList']);
   }
 
   goBack() {
-
-    this.router.navigate(['pages/fundList'])
+    this.router.navigate(['pages/fundList']);
   }
 }
--- src/app/service/cust-card-in.service.ts
+++ src/app/service/cust-card-in.service.ts
@@ -6,7 +6,20 @@ import { Injectable } from '@angular/core';
 export class CustCardInService {
 
   dataSource1: any[] = [];
-  radioValue = {}
+  radioValue = {
+    annualIncome: '',
+    financialAssets: '',
+    investmentPolicy: '',
+    InvestmentExperience_1: '',
+    InvestmentExperience_2: '',
+    InvestmentExperience_3: '',
+    InvestmentExperience_4: '',
+    InvestmentExperience_5: '',
+    InvestmentExperience_6: '',
+    InvestmentExperience_7: '',
+    InvestmentExperience_8: '',
+    InvestmentExperience_9: '',
+  }
 
   constructor() { }
 }
